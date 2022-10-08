24c9079ec92d0c6b1a633f6f791dc223d1cc9894
scottsak <48599109+scottsak@users.noreply.github.com>
2022-10-07 22:05:19 -0700

    fixed the search function

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,52 +1,56 @@
 import React, { Component, Fragment, useState } from "react";
 
 const Autocomplete = ({ suggestions, win, setWin, song, gamesPlayed, setGamesPlayed }) => {
-    const [input, setInput] = useState("");
+
     const [filteredSuggestions, setFilteredSuggestions] = useState([]);
     const [showSuggestions, setShowSuggestions] = useState(false);
+    const [input, setInput] = useState("");
+
 
     const checkAns = (guess) => {
         if (guess.toLowerCase() === song) {
             localStorage.setItem('win', 'won');
             setWin('won');
             let tempWins = JSON.parse(localStorage.getItem('wins'));
-            if(!tempWins){
-              tempWins = 0;
+            if (!tempWins) {
+                tempWins = 0;
             }
-            localStorage.setItem('wins',  parseInt(tempWins)+1)
-            localStorage.setItem('gamesPlayed', parseInt(JSON.parse(localStorage.getItem('gamesPlayed')))+1);
-            setGamesPlayed(gamesPlayed+1)
+            localStorage.setItem('wins', parseInt(tempWins) + 1)
+            localStorage.setItem('gamesPlayed', parseInt(JSON.parse(localStorage.getItem('gamesPlayed'))) + 1);
+            setGamesPlayed(gamesPlayed + 1)
         }
         else {
             localStorage.setItem('win', 'lost');
             setWin('lost');
             let tempLosses = JSON.parse(localStorage.getItem('losses'));
-            if(!tempLosses){
-              tempLosses = 0;
+            if (!tempLosses) {
+                tempLosses = 0;
             }
-            localStorage.setItem('losses',  parseInt(tempLosses)+1)
-            localStorage.setItem('gamesPlayed', parseInt(JSON.parse(localStorage.getItem('gamesPlayed')))+1);
-            setGamesPlayed(gamesPlayed+1)
+            localStorage.setItem('losses', parseInt(tempLosses) + 1)
+            localStorage.setItem('gamesPlayed', parseInt(JSON.parse(localStorage.getItem('gamesPlayed'))) + 1);
+            setGamesPlayed(gamesPlayed + 1)
         }
     }
 
     const onChange = (e) => {
+        const userInput = e.target.value;
 
         // Filter our suggestions that don't contain the user's input
-        const filteredSuggestion = suggestions.filter(
-            suggestion =>
-                suggestion.toLowerCase().indexOf(input.toLowerCase()) > -1
+        const unLinked = suggestions.filter(
+            (suggestion) =>
+                suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
         );
 
-        setFilteredSuggestions(filteredSuggestion);
         setInput(e.target.value);
+        setFilteredSuggestions(unLinked);
         setShowSuggestions(true);
     };
 
-    const onClick = (e, key) => {
+    const onClick = (e) => {
         setFilteredSuggestions([]);
-        setInput("");
+        setInput(e.target.innerText);
         checkAns(e.target.innerText);
+        setShowSuggestions(false);
     };
 
 
@@ -57,20 +61,24 @@ const Autocomplete = ({ suggestions, win, setWin, song, gamesPlayed, setGamesPla
                 {filteredSuggestions.map((suggestion, index) => {
                     let className;
 
+                    // Flag the active suggestion with a 
+
                     return (
-                        <li className={className} key={suggestion} onClick={event => onClick(event, suggestion)}>
+                        <li className={className} key={suggestion} onClick={onClick}>
                             {suggestion}
                         </li>
                     );
                 })}
             </ul>
         ) : (
-            <div className="no-suggestions">
+            <div class="no-suggestions">
+                <span role="img" aria-label="tear emoji">
+                    😪
+                </span>{" "}
                 <em>sorry no suggestions</em>
             </div>
         );
     };
-
 
     return (
         <>
@@ -89,7 +97,98 @@ const Autocomplete = ({ suggestions, win, setWin, song, gamesPlayed, setGamesPla
             </div>
         </>
     );
-}
-
+};
 
 export default Autocomplete;
+
+//     const [input, setInput] = useState("");
+//     const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+//     const [showSuggestions, setShowSuggestions] = useState(false);
+
+//     const checkAns = (guess) => {
+//         if (guess.toLowerCase() === song) {
+//             localStorage.setItem('win', 'won');
+//             setWin('won');
+//             let tempWins = JSON.parse(localStorage.getItem('wins'));
+//             if(!tempWins){
+//               tempWins = 0;
+//             }
+//             localStorage.setItem('wins',  parseInt(tempWins)+1)
+//             localStorage.setItem('gamesPlayed', parseInt(JSON.parse(localStorage.getItem('gamesPlayed')))+1);
+//             setGamesPlayed(gamesPlayed+1)
+//         }
+//         else {
+//             localStorage.setItem('win', 'lost');
+//             setWin('lost');
+//             let tempLosses = JSON.parse(localStorage.getItem('losses'));
+//             if(!tempLosses){
+//               tempLosses = 0;
+//             }
+//             localStorage.setItem('losses',  parseInt(tempLosses)+1)
+//             localStorage.setItem('gamesPlayed', parseInt(JSON.parse(localStorage.getItem('gamesPlayed')))+1);
+//             setGamesPlayed(gamesPlayed+1)
+//         }
+//     }
+
+//     const onChange = (e) => {
+
+//         // Filter our suggestions that don't contain the user's input
+//         const filteredSuggestion = suggestions.filter(
+//             suggestion =>
+//                 suggestion.toLowerCase().indexOf(input.toLowerCase()) > -1
+//         );
+
+//         setFilteredSuggestions(filteredSuggestion);
+//         setInput(e.target.value);
+//         setShowSuggestions(true);
+//     };
+
+//     const onClick = (e) => {
+//         setFilteredSuggestions([]);
+//         setInput("");
+//         checkAns(e.target.innerText);
+//         setShowSuggestions(false)
+//     };
+
+//     const SuggestionsListComponent = () => {
+//         return filteredSuggestions.length ? (
+//             <ul className="suggestions">
+//                 {filteredSuggestions.map((suggestion, index) => {
+//                     let className;
+
+//                     return (
+//                         <li className={className} key={suggestion} onClick={onClick}>
+//                             {suggestion}
+//                         </li>
+//                     );
+//                 })}
+//             </ul>
+//         ) : (
+//             <div className="no-suggestions">
+//                 <em>sorry no suggestions</em>
+//             </div>
+//         );
+//     };
+
+
+//     return (
+//         <>
+//             {win === 'won' || win === 'lost' ? <div className="lossSpace"></div> : <input
+//                 placeholder={"Guess the song title"}
+//                 type="text"
+//                 onChange={onChange}
+//                 value={input}
+//                 className="guess-section"
+//             />}
+
+//             <div className="autocompleteWrapper">
+//                 <div className="suggestionsWrapper">
+//                     {showSuggestions && input && <SuggestionsListComponent />}
+//                 </div>
+//             </div>
+//         </>
+//     );
+// }
+
+
+// export default Autocomplete;
